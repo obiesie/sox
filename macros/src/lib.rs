d864937e6260c2aa9101733cf057924345d31bf1
obiesie <obiesie_c@yahoo.com>
2024-04-28 15:17:03 +0100

    Add proc macro crate to experiment with generating code using macros.

---
+++ macros/src/lib.rs
@@ -0,0 +1,31 @@
+use proc_macro::{self, TokenStream};
+
+use quote::{quote, ToTokens};
+use syn::{ItemFn, parse_macro_input};
+use syn::Item;
+
+use crate::proc::soxtypeimpl;
+
+mod proc;
+
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[proc_macro_attribute]
+pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    TokenStream::from(quote!(#input))
+}
+
+#[proc_macro_attribute]
+pub fn soxtype(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let p_item = parse_macro_input!(item as Item);
+    soxtypeimpl(p_item).into()
+}
+
+#[proc_macro_attribute]
+pub fn soxmethod(attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
---
+++ macros/src/proc.rs
@@ -0,0 +1,51 @@
+use proc_macro2::{Delimiter, Group, TokenStream, TokenTree};
+use quote::{quote, ToTokens};
+use syn::{ImplItem, Item};
+
+pub fn soxtypeimpl(item: Item) -> TokenStream {
+    let mut methods = Vec::new();
+    
+    let tokens = match item.clone() {
+        Item::Impl(item_impl) => {
+
+            let ident = item_impl.self_ty.as_ref().into_token_stream(); 
+            
+            for i in item_impl.items.iter() {
+                match i {
+                    
+                    ImplItem::Fn(v) => {
+                        let fn_name = v.sig.ident.clone();
+
+                        if v.attrs.len() > 0 {
+                            methods.push((fn_name.to_string(), fn_name));
+                        }
+                    }
+                   
+                    
+                    _ => {}
+                }
+            }
+            let mut inner_tokens = TokenStream::new();
+            let mut tokens = TokenStream::new();
+            for (method_name, method) in methods {
+                inner_tokens.extend(quote! [
+                    (#method_name, SoxMethod{
+                        
+                        func: static_func(#ident::#method)
+                    }),
+                ]);
+            };
+            let array: TokenTree = Group::new(Delimiter::Bracket, inner_tokens).into();
+            tokens.extend([array]);
+
+            quote! {
+                #item_impl
+                impl SoxClassImpl for #ident{
+                    const METHOD_DEFS: &'static [(&'static str, SoxMethod)] = &#tokens;
+                }
+            }
+        }
+        _ => item.into_token_stream()
+    };
+    tokens.into_token_stream()
+}
