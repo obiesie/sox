c05c0c3957fb3a08dd8a353169cd8c682eeaa17c
obiesie <obiesie_c@yahoo.com>
2024-04-29 21:00:43 +0100

    Add type catalog for holding and initialising builtin types on interpreter startup.

---
+++ src/catalog.rs
@@ -0,0 +1,28 @@
+use crate::builtins::{bool_, exceptions, float, function, int, none, string};
+use crate::core::{SoxType, StaticType};
+
+#[derive(Debug)]
+pub struct TypeLibrary{
+    pub bool_type: &'static SoxType,
+    pub float_type: &'static SoxType,
+    pub int_type: &'static SoxType,
+    pub str_type: &'static SoxType,
+    pub none_type: &'static SoxType,
+    pub exception_type: &'static SoxType,
+    pub func_type: &'static SoxType,
+    
+}
+
+impl TypeLibrary{
+    pub fn init() -> Self {
+        Self{
+            bool_type: bool_::SoxBool::init_builtin_type(),
+            float_type: float::SoxFloat::init_builtin_type(),
+            int_type: int::SoxInt::init_builtin_type(),
+            str_type: string::SoxString::init_builtin_type(),
+            none_type: none::SoxNone::init_builtin_type(),
+            exception_type: exceptions::Exception::init_builtin_type(),
+            func_type: function::SoxFunction::init_builtin_type()
+        }
+    }
+}
