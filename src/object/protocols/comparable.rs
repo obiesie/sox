a7eb7c449921c9d5718922b5efea672d41524143
obiesie <obiesie_c@yahoo.com>
2025-01-25 23:14:14 +0000

    Add comparable module.

---
+++ src/object/protocols/comparable.rs
@@ -0,0 +1,24 @@
+use crate::builtins::core::SoxResult;
+use crate::interpreter::Interpreter;
+use crate::object::core::SoxObjectRef;
+
+pub trait Comparable{
+    
+    fn as_comparable() -> ComparableMethods;
+
+
+    
+
+}
+
+
+#[derive(Clone, Debug, Default)]
+pub struct ComparableMethods {
+    pub lt: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+    pub gt: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+    pub eq: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+    pub ne: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+    pub ge: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+    pub le: Option<fn(a: SoxObjectRef, b: SoxObjectRef, i: &Interpreter) -> SoxResult>,
+
+}
